92cfc5500a57108cc32919013d671a074a264f84
htzhao <htzhao_001@163.com>
2020-06-25 22:38:50 +0800

    完成页面元素

---
+++ src/main/java/com/htzhao/po/page/baiduPage.java
@@ -0,0 +1,10 @@
+package com.htzhao.po.page;
+
+import org.openqa.selenium.By;
+
+public class baiduPage {
+    public static By searchInput = By.id("kw");
+    public static By searchButton = By.id("su");
+
+
+}
